89ae7b1ff2e05ab71ca3df6c14151c50ea0e70bc
iceymoss <ice_moss@163.com>
2023-10-23 19:39:29 +0800

    feat:完成电影列表，电影详情的开发

--- common/libs/Helper.py
+++ common/libs/Helper.py
@@ -1,3 +1,5 @@
+import math
+
 from flask import jsonify, g, render_template
 
 
@@ -16,3 +18,26 @@ def ops_renderJSON(code=200, msg="操作成功", data={}):
 
 def ops_renderErrJSON(msg="系统繁忙，请稍后再试", data={}):
     return ops_renderJSON(code=-1, msg=msg, data=data)
+
+def iPagenation( params):
+    total_count = int( params['total_count'] )
+    page_size = int( params['page_size'] )
+    page = int( params['page'] )
+
+    total_pages = math.ceil(total_count / page_size)
+    total_pages = total_pages if total_pages > 0 else 1
+
+    is_prev = 0 if page <= 1 else 1
+    is_next = 0 if page >= total_pages else 1
+    pages = {
+        'current':page,
+        'total_pages':total_pages,
+        'total':total_count,
+        'page_size':page_size,
+        'is_next': is_next,
+        'is_prev': is_prev,
+        'range': range( 1,total_pages + 1 ),
+        'url':params['url']
+    }
+
+    return pages
--- controllers/index.py
+++ controllers/index.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+import random
+from flask import Blueprint, request, redirect
+from sqlalchemy import desc, between
 from common.libs.Helper import ops_gender
+from common.libs.Helper import iPagenation
+from common.libs.UrlManager import UrlManager
 from common.models.user import User
 from application import db, global_db
+from common.models import movie
+
+
 
 # 蓝图优化
 index_page = Blueprint("index_page", __name__)
@@ -10,8 +17,61 @@ index_page = Blueprint("index_page", __name__)
 
 @index_page.route("/")
 def index():
+    req = request.values
 
-    context = {}
-    return ops_gender("index.html", context)
+    # 获取最新数据
+    order_by_f = str(req['order']) if ("order" in req and req['order']) else "lastest"
+    page = 1
+    if 'p' in req and req['p']:
+        page = int(req['p'])
+
+    query = global_db.query(movie.Movie)
+    print("参数：", order_by_f)
+
+    page_params = {
+        'total_count': int(query.count()),
+        "page_size": 30,
+        'page': page,
+        'url': "/?"
+    }
+
+    pages = iPagenation(page_params)
+    # 0 - 30,30 - 60 ,60 - 90
+    offset = (page - 1) * page_params['page_size']
+    limit = page * page_params['page_size']
+
+    if order_by_f == "hot":
+        query = query.order_by(desc(movie.Movie.view_counter))
+    else:
+        query = query.order_by(movie.Movie.pub_date.desc(), movie.Movie.id.desc())
+    list_movie = query.offset(offset).limit(limit).all()
+    # for item in list_movie:
+    #     print("返回", item.__str__())
+    return ops_gender("index.html", { "data":list_movie,"pages":pages })
+
+@index_page.route("/info")
+def get_movieinfo():
+    req = request.values
+    movie_id = int(req["id"]) if ("id" in req and req["id"]) else 0
+    if movie_id < 1:
+        return redirect(UrlManager.buildUrl("/"))
+
+    info = global_db.query(movie.Movie).filter_by(id=movie_id).first()
+    if info is None:
+        return redirect(UrlManager.buildUrl("/"))
+
+    # 增加点击量
+    info.view_counter += 1
+    recommend = info.view_counter
+
+    # 提交更改到数据库
+    global_db.commit()
+
+    # 获取推荐内容
+    start = recommend-100
+    end = recommend+100
+    recommend_list = global_db.query(movie.Movie).filter(movie.Movie.view_counter.between(start, end)).all()
+    sampled_elements = random.sample(recommend_list, 4)
+    return ops_gender("info.html", {"info": info, "recommend_list": sampled_elements})
 
 
